04300b9e440ee7315b7354b3328b7213a98dea15
dancingkimDH <kimdonghyun0701@gmail.com>
2023-08-01 18:16:25 +0900

    update

---
+++ datatype_list.py
@@ -0,0 +1,22 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+pass
+
+# check datatype
+# type(thislist)
+# <class 'list'>
+
+# len(thislist)  => 순열방식에서 작동 e.g. list, string, etc
+# 7
+
+# thislist[1:3] => -1을 해서 1, 2만 나옴
+# ['banana', 'cherry']
+
+# thislist[:-1]
+# ['apple', 'banana', 'cherry', 'orange', 'kiwi', 'melon']
+
+#  ---------
+
+thislist[1] = "watermelon"
+pass
+
+# thislist[1:3] = ["cherry", "watermelon"] => 둘의 순서를 바꿈
