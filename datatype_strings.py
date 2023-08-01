c0f86f93822b53f4c74c763b12f7a7a3c74c2e8f
dancingkimDH <kimdonghyun0701@gmail.com>
2023-08-01 17:07:44 +0900

    upload

---
+++ datatype_strings.py
@@ -0,0 +1,12 @@
+str = "Yojulab !"
+len(str)
+pass
+
+# debugging console
+# "ju" in str
+# True
+# "let" in str
+# False
+# "ju" not in str
+# False
+
---
+++ syntaxs.py
@@ -0,0 +1,7 @@
+# tab & variable
+
+first = 2
+second = 5
+if first > second:
+  print("Five is greater than two!")
+print('Yojulab!')
