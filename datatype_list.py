c6066d8864d81be64ac87f45f0e326188e713e51
dancingkimDH <kimdonghyun0701@gmail.com>
2023-08-04 09:45:28 +0900

    update

--- datatype_list.py
+++ datatype_list.py
@@ -21,4 +21,4 @@ pass
 
 # thislist[1:3] = ["cherry", "watermelon"] => 둘의 순서를 바꿈
 # thislist.sort() => 아이템 정렬, quick sort
-# thislist.sort(reverse=True) => descending 정렬
+# thislist.sort(reverse=True) => descending 정렬, 이때 T 대문자 주의
---
+++ tryme.py
@@ -0,0 +1,23 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+pass
+
+string = "Yojulab !"
+len(string)
+pass
+
+string[3]
+pass
+
+age = 36
+txt = "My name is John, I am " + str(age)
+print(txt)
+pass
+
+quantity = 3 # index 0
+itemno = 567 # index 1
+price = 49.95 # index 2 
+myorder = "I want {0} pieces of item {1} for {2} dollars. I have just {2}."
+pass
+
+mysecondorder = "I want {0} pieces of pizza. It costs {1} dollars, right?"
+pass
