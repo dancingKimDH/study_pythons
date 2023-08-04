dfe7526119d84323de4e5f4ba07e152b8db92589
dancingkimDH <kimdonghyun0701@gmail.com>
2023-08-04 10:51:31 +0900

    updaste

--- datatype_list.py
+++ datatype_list.py
@@ -20,5 +20,22 @@ thislist[1] = "watermelon"
 pass
 
 # thislist[1:3] = ["cherry", "watermelon"] => 둘의 순서를 바꿈
+
 # thislist.sort() => 아이템 정렬, quick sort
 # thislist.sort(reverse=True) => descending 정렬, 이때 T 대문자 주의
+
+
+# ------------------- 자바의 ;, {}, 변수의 datatype 규칙과 달리 Python은 tab만 주의
+thislist = ["apple", "banana", "cherry"]
+
+# thislist.append("melon") : 새로운 아이템을 뒤에 추가
+# thislist.pop() : 끝에 있는 아이템이 삭제
+
+pass
+
+# thislist = []
+# thislist = list () : 리스트 초기화, 실무에서는 이 방식이 더 활용
+
+# x = str("s1") : casting e.g. int, float, string, etc.
+
+
---
+++ loops.py
@@ -0,0 +1,16 @@
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+# for item in items:
+#     pass
+
+for fruit in thislist:
+    print(fruit)
+pass
+
+# range(2, 10)
+# for(int i = 0; i < 10; i ++) {} : 아래 구문을 자바로 작성할 시
+
+for i in range(2, 10):
+    print(i)
+
+pass
